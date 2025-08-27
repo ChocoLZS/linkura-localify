86cb3577214521821ec9ec92d6bf0bc9541f01bb
ChocoLZS <chocoielzs@gmail.com>
2025-08-27 19:11:47 +0800

    fix: json parse error

--- app/src/main/java/io/github/chocolzs/linkura/localify/mainUtils/JsonHelper.kt
+++ app/src/main/java/io/github/chocolzs/linkura/localify/mainUtils/JsonHelper.kt
@@ -1,7 +1,12 @@
 package io.github.chocolzs.linkura.localify.mainUtils
 
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 
+@OptIn(ExperimentalSerializationApi::class)
 val json = Json {
     encodeDefaults = true
+    ignoreUnknownKeys = true
+    allowTrailingComma = true
+    allowComments = true
 }
